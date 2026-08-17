f5efe90f1cffbbf3c918e559e27892ea21a8f8c2
Le Van Tuan Em <levantuanem00@gmail.com>
2026-08-17 22:03:21 +0700

    chore: clean old project files

--- src/features/build_features.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import joblib
-from sklearn.preprocessing import StandardScaler
-
-def build_features():
-    data = pd.read_csv(r'D:\AI_Projects\Project_1\data\processed\diabetes_clean.csv', sep= ';')
-    x = data.drop("Outcome", axis=1)
-    y = data["Outcome"]
-    return x, y
--- src/models/predict.py
+++
@@ -1,8 +0,0 @@
-def predict():
-    # Load scaler
-    # Load model
-    # Predict dữ liệu mới
-    return 
-
-if __name__ == "__main__":
-    predict()
